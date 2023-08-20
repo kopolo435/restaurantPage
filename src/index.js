cd9a8fec4788cf2c253291633f71bffaeecbc0a4
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-08-20 14:51:50 -0500

    Agrega creacion de content container

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './style.css'
 import initialLoad from './initialLoad.js'
 
-initialLoad();
+const divContent = document.createElement("div");
+divContent.setAttribute("id","content");
+document.body.appendChild(divContent); 
+initialLoad(divContent);
+
 console.log("Mi nombre es Samir Fanilla");
