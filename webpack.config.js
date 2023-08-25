7cc48797a791cf5c61b1d8a557a05f39a24d86f6
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-08-24 19:23:56 -0500

    Agrega configuracion para un server

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  optimization: {
+    runtimeChunk: 'single',
+  },
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   module: {
     rules: [
       {
