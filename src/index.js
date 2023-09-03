662123693d7973e1d67b64df8cc43b34042ca458
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-09-03 17:37:46 -0500

    Agrega codigo para añadir elementos a pagina

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './style.css'
 import initialLoad from './initialLoad.js'
+import * as pageComponent from './pageComponents.js';
 
 const divContent = document.createElement("div");
 divContent.setAttribute("id","content");
+document.body.appendChild(pageComponent.loadHeader());
 document.body.appendChild(divContent); 
 initialLoad(divContent);
+document.body.appendChild(pageComponent.loadNavBar());
+document.body.appendChild(pageComponent.createFooter());
 
 console.log("Mi nombre es Samir Fanilla");
