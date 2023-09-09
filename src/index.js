9e9293304654ddd9bc7e28a0ee2f07311fafd3c7
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-09-09 12:58:47 -0500

    Agrega importacion de modulo de menu.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css'
 import initialLoad from './initialLoad.js'
+import * as menu from './menu.js'
 import * as pageComponent from './pageComponents.js';
 
 const divContent = document.createElement("div");
@@ -9,5 +10,8 @@ document.body.appendChild(divContent);
 initialLoad(divContent);
 document.body.appendChild(pageComponent.loadNavBar());
 document.body.appendChild(pageComponent.createFooter());
+
+divContent.replaceChildren();
+divContent.appendChild(menu.getMenuContainer());
 
 console.log("Mi nombre es Samir Fanilla");
