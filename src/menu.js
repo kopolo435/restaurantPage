d8c54b2346841e4cffd19e7336a904ebdc756466
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-09-09 11:41:58 -0500

    Crea factory Plate y modulo para controlar menu
    Crea una factoryFunction para crear objetos de tipo plate, los cuales
    compondran el menu que almacenara todos los plates del restaurante

---
+++ src/menu.js
@@ -0,0 +1,25 @@
+const createPlate = (name,price,photoURl)=>{
+
+    return{name,price,photoURl}
+}
+
+const menuItems = [["pollo",20.25,"url"],["arroz",10.23,"urlArroz"]];
+
+const menuController = (()=>{
+    const menu = [];
+    const createMenu = (menuItems)=>{
+        menuItems.forEach(plateInfo =>{
+            menu.push(createPlate(plateInfo[0],plateInfo[1],plateInfo[2]));
+        });
+    }
+
+    const getMenu = ()=>{
+        return menu;
+    }
+    return {getMenu,createMenu};
+
+})();
+
+function createPlateDescription(){
+    
+}
