0161220cbf45daa4c7ba1ecd8d23760b7bf8753a
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-09-09 13:56:13 -0500

    Agrega importacion del modulo de contact

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css'
 import initialLoad from './initialLoad.js'
 import * as menu from './menu.js'
+import * as contact from './contact.js'
 import * as pageComponent from './pageComponents.js';
 
 const divContent = document.createElement("div");
@@ -12,6 +13,6 @@ document.body.appendChild(pageComponent.loadNavBar());
 document.body.appendChild(pageComponent.createFooter());
 
 divContent.replaceChildren();
-divContent.appendChild(menu.getMenuContainer());
+divContent.appendChild(contact.getContactContainer());
 
 console.log("Mi nombre es Samir Fanilla");
