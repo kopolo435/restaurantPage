6498f372bbe71095f9b97265c8e23bb6c462f5f9
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-10-02 17:05:47 -0500

    Importa las imagenes de home y agrega su src

--- src/initialLoad.js
+++ src/initialLoad.js
@@ -1,13 +1,16 @@
-/*import breakfast from './img/breakfast.jpg'*/
+import edificio from './img/edificio.jpg'
+import shirakawa from './img/shirakawa.jpg'
+import bar from './img/bar.jpg'
+import comida from './img/sushi-rollos.jpg'
 
 const createSection = (title,description,image)=>{
     return {title,description,image}
 }
 
-const sectionDataArray = [["Bienvenidos a Best Taste","Descripcion Presentacion que hable del restaurante","Imagen edifico"],
-["Ubicacion","Descripcion de el mensaje de bienvenida","Imagen de shirakawa"],
-["Apariencia Local","descripcion de que tan grandiosas son las instalaciones","imagen del bar"],
-["Comida","descripcion de que tan grandiosa es la comida ","imagen de comida"],
+const sectionDataArray = [["Bienvenidos a Best Taste","Descripcion Presentacion que hable del restaurante",edificio],
+["Ubicacion","Descripcion de el mensaje de bienvenida",shirakawa],
+["Apariencia Local","descripcion de que tan grandiosas son las instalaciones",bar],
+["Comida","descripcion de que tan grandiosa es la comida ",comida],
 ]
 
 const sectionArray = []
